5399af104da694a39ee88b810ebd31fb760616f9
CJL <2662442385@qq.com>
2024-03-10 15:50:39 +0800

    更改导出方式

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,16 @@
-export {
-    getType,
-    randomStr,
-    dayOfYear,
-    timeFromDate,
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    dayDiff,
-    getRandomNum,
-    getSum,
-    deepClone,
-    deepCompare,
-    arrToTree,
-    arrToChunk,
-    binarySearch,
-    cutStr,
-    toCamel,
-    curry,
-    padNum,
-    padDate,
-    getValidDate,
-    isLtYear,
-    genIcon,
-    numFixed,
-    filterVals,
-    excludeVals,
-    filterKeys,
-    excludeKeys,
-    EventBus
-} from '@/tools/tools'
-
-export {
-    adaptPx,
-    handleCssUnit,
-    pxToVw,
-    getStyle,
-    throttle,
-    debounce,
-    getSelectedText,
-    copyToClipboard,
-    isDarkMode,
-    isToBottom,
-    getAllStyle,
-    print,
-    judgeImgLoad,
-    getImg,
-    blobToBase64,
-    doubleKeyDown,
-    setParentOverflow,
-    fullScreen,
-    HTMLToStr,
-    getWinHeight,
-    getWinWidth,
-    downloadByData,
-    matchProtocol,
-    downloadByUrl
-} from '@/tools/domTools'
+export * from '@/tools/tools'
+export * from '@/tools/domTools'
+export * from '@/tools/color'
+export * from '@/shared/exportToDoc'
+export * from '@/dataStructure'
 
 export { scheduleTask } from '@/tools/scheduleTask'
 
-export {
-    Recorder,
-    Speaker,
-    SpeakToTxt,
-    openCamera,
-    screenCAP
-} from '@/webApi'
+export * from '@/webApi'
+export * from '@/animation'
 
-export {
-    getColor,
-    getColorArr,
-    rgbToHex,
-    hexToRGB,
-    hexColorToRaw,
-    lightenColor,
-    colorAddOpacity
-} from '@/tools/color'
-
-export {
-    MinHeap,
-    MaxHeap
-} from '@/dataStructure'
-
-export {
-    applyAnimation,
-    createAnimation,
-    createAnimationByTime,
-    genTimeFunc,
-    ATo
-} from '@/animation'
-
-export {
-    TimeType,
-    KeyCode,
-    TreeData,
-    OnUpdate,
-    AnimationOpt,
-    TimeFunc,
-    BaseType
-} from '@/types'
-
-export {
-    Reg,
-    isPureNum,
-    isArr,
-    isBool,
-    isNum,
-    isObj,
-    isStr,
-    isSame,
-    isFn
-} from '@/shared'
-
-
-export {
-    calcCoord,
-    createCvs,
-    getPixel,
-    fillPixel,
-    parseImgData,
-    cutImg,
-    compressImg
-} from '@/canvas'
+export * from '@/types'
+export * from '@/canvas'
 
 export {
     autoUpdate
