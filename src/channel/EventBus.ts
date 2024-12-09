3ed89b4ac5b9a30ce32b5c32ecc827e5a6caa860
ES <2662442385@qq.com>
2024-12-09 18:17:55 +0800

    EventBus可接收遗漏事件

--- src/channel/EventBus.ts
+++ src/channel/EventBus.ts
@@ -5,10 +5,18 @@ import type { BaseKey } from '../types'
  * 消息订阅与派发，订阅和派发指定消息
  */
 export class EventBus<T extends BaseKey = BaseKey> {
+
   private readonly eventMap = new Map<BaseKey, Set<{
     once?: boolean
     fn: Function
   }>>()
+  private readonly beforeTriggerMap = new Map<BaseKey, any[]>()
+
+  opts: Required<EventBusOpts>
+
+  constructor(opts: EventBusOpts = {}) {
+    this.opts = mergeOpts(opts)
+  }
 
   /**
    * 订阅
@@ -35,9 +43,22 @@ export class EventBus<T extends BaseKey = BaseKey> {
    */
   emit(eventName: T, ...args: any[]) {
     const fnSet = this.eventMap.get(eventName)
-    if (!fnSet) {
+
+    /**
+     * 没有事件接受，先存起来
+     */
+    if (!fnSet && this.opts.triggerBefore) {
+      const params = this.beforeTriggerMap.get(eventName)
+      if (params) {
+        params.push(args)
+      }
+      else {
+        this.beforeTriggerMap.set(eventName, [args])
+      }
       return
     }
+
+    if (!fnSet) return
 
     fnSet.forEach(({ fn, once }) => {
       fn(...args)
@@ -51,13 +72,17 @@ export class EventBus<T extends BaseKey = BaseKey> {
    * @param func 要取关的函数，为空取关该事件的所有函数
    */
   off(eventName?: T, func?: Function) {
+    // 不传重置所有
     if (!eventName) {
       this.eventMap.clear()
+      this.beforeTriggerMap.clear()
       return
     }
 
     const fnSet = this.eventMap.get(eventName)
-    /** fn 为空取关该事件的所有函数 */
+    /** 
+     * fn 为空取关该事件的所有函数
+     */
     if (fnSet && !func) {
       fnSet.clear()
       return
@@ -68,6 +93,8 @@ export class EventBus<T extends BaseKey = BaseKey> {
         fnSet.delete(item)
       }
     })
+
+    this.beforeTriggerMap.delete(eventName)
   }
 
   private subscribe(eventName: T, fn: Function, once = false) {
@@ -79,9 +106,33 @@ export class EventBus<T extends BaseKey = BaseKey> {
     this.eventMap
       .get(eventName)!
       .add(EventBus.genItem(fn, once))
+
+    /** 
+     * 如果有之前遗漏事件，则统一派发事件
+     */
+    const args = this.beforeTriggerMap.get(eventName)
+    if (args) {
+      args.forEach(arg => fn(...(arg || [])))
+      this.beforeTriggerMap.delete(eventName)
+    }
   }
 
   private static genItem(fn: Function, once = false) {
     return { fn, once }
   }
+}
+
+function mergeOpts(opts: EventBusOpts = {}) {
+  const defaultOpts: Required<EventBusOpts> = { triggerBefore: false }
+  return Object.assign(defaultOpts, opts)
+}
+
+
+export type EventBusOpts = {
+  /**
+   * 是否触发遗漏的事件
+   * 当 emit 没有被订阅时，后续订阅者会收到
+   * @default false
+   */
+  triggerBefore?: boolean
 }
--- test/test/channel/channel.test.ts
+++ test/test/channel/channel.test.ts
@@ -3,68 +3,118 @@ import { EventBus, Observer, type IObserver } from '@/channel'
 
 
 describe('消息发布与订阅测试', () => {
-    const eb = new EventBus()
+  const eb = new EventBus()
 
-    const testArgsFn = vi.fn(() => (...args: any[]) => args),
-        testFn2 = vi.fn(() => 'test2'),
-        testFn3 = vi.fn(() => 'test3'),
-        testFn4 = vi.fn(() => 'test4')
+  const
+    testArgsFn = vi.fn(() => (...args: any[]) => args),
+    onceFn = vi.fn(() => 'once'),
 
-
-    eb.on('testArgsFn', testArgsFn)
-    eb.on('test2', testFn2)
-    eb.on('test2', testFn3)
-    eb.on('test2', testFn4)
-    eb.once('once', () => console.log('once'))
+    testFn2 = vi.fn(() => 'test2'),
+    testFn3 = vi.fn(() => 'test3'),
+    testFn4 = vi.fn(() => 'test4')
 
 
-    eb.emit('testArgsFn', 1, 2, 3)
-    test('事件触发测试', () => {
-        expect(testArgsFn.mock.calls).toEqual([[1, 2, 3]])
-    })
+  eb.on('testArgsFn', testArgsFn)
+
+  eb.on('test2', testFn2)
+  eb.on('test2', testFn3)
+  eb.on('test2', testFn4)
+  eb.once('once', onceFn)
 
 
-    eb.emit('once')
-    eb.emit('once')
-    test('仅仅触发一次', () => {
-        expect(testArgsFn).toHaveBeenCalledTimes(1)
-    })
+  eb.emit('testArgsFn', 1, 2, 3)
+  test('参数接收测试', () => {
+    expect(testArgsFn.mock.calls).toEqual([[1, 2, 3]])
+  })
 
 
-    /** 取关 testFn2 */
-    eb.off('test2', testFn2)
+  eb.emit('once')
+  eb.emit('once')
+  test('仅仅触发一次', () => {
+    expect(onceFn).toHaveBeenCalledTimes(1)
+  })
 
 
-    eb.emit('test2')
-    eb.emit('test2')
-    test('触发两次', () => {
-        expect(testFn2).not.toBeCalled()
-        expect(testFn3).toHaveBeenCalledTimes(2)
-        expect(testFn4).toHaveBeenCalledTimes(2)
-    })
+  /** 取关 testFn2 */
+  eb.off('test2', testFn2)
+
+
+  eb.emit('test2')
+  eb.emit('test2')
+  test('触发两次', () => {
+    expect(testFn2).not.toBeCalled()
+    expect(testFn3).toHaveBeenCalledTimes(2)
+    expect(testFn4).toHaveBeenCalledTimes(2)
+  })
+})
+
+
+describe('接受遗漏事件测试', () => {
+  const eb = new EventBus({ triggerBefore: true })
+
+  const testFn = vi.fn(() => 'testFn')
+
+  eb.emit('test', testFn)
+  eb.on('test', testFn)
+
+  test('接受遗漏事件', () => {
+    expect(testFn).toHaveBeenCalledTimes(1)
+  })
+
+
+  const argsFn = vi.fn(() => (...args: any[]) => args)
+  eb.emit('testArgsFn', 1, 2, 3)
+  eb.on('testArgsFn', argsFn)
+
+  test('参数接收测试', () => {
+    expect(argsFn.mock.calls).toEqual([[1, 2, 3]])
+  })
 })
 
 
 
+describe('不接受遗漏事件测试', () => {
+  const eb = new EventBus({ triggerBefore: false })
+
+  const testFn = vi.fn(() => 'testFn')
+
+  eb.emit('test', testFn)
+  eb.on('test', testFn)
+
+  test('接受遗漏事件', () => {
+    expect(testFn).toHaveBeenCalledTimes(0)
+  })
+
+
+  const argsFn = vi.fn(() => (...args: any[]) => args)
+  eb.emit('testArgsFn', 1, 2, 3)
+  eb.on('testArgsFn', argsFn)
+
+  test('参数接收测试', () => {
+    expect(argsFn).toHaveBeenCalledTimes(0)
+  })
+})
+
+
 describe('观察者模式', () => {
-    const data: number[] = [],
-        ob = new Observer()
+  const data: number[] = [],
+    ob = new Observer()
 
-    const o1: IObserver = {
-        update: () => data.push(1)
-    }
-    const o2: IObserver = {
-        update: () => data.push(2)
-    }
+  const o1: IObserver = {
+    update: () => data.push(1)
+  }
+  const o2: IObserver = {
+    update: () => data.push(2)
+  }
 
-    ob.addObserver(o1)
-    ob.addObserver(o2)
-    ob.removeObserver(o1)
+  ob.addObserver(o1)
+  ob.addObserver(o2)
+  ob.removeObserver(o1)
 
-    ob.notify()
-    ob.notify()
+  ob.notify()
+  ob.notify()
 
-    test('观测数据', () => {
-        expect(data).toEqual([2, 2])
-    })
+  test('观测数据', () => {
+    expect(data).toEqual([2, 2])
+  })
 })
